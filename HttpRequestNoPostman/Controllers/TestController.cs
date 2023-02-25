f9e0d75fa9d12684f25a784e697a536e34363339
Oğuzhan <Ouzhan@DESKTOP-MBGVKF7>
2023-02-26 02:10:07 +0300

    Add project files.

---
+++ HttpRequestNoPostman/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HttpRequestNoPostman.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+
+            string test1 = "Şuan sinirden gülüyorum  yemin ederim... !! ";
+            return test1;
+        }
+
+        [HttpPost]
+        public string Post([FromBody] string NAME ) 
+        {
+
+            string CUMLE = $"{NAME} naberrrrrrrrrrr";
+
+            return CUMLE;
+            
+        
+        
+        }
+
+    }
+}
